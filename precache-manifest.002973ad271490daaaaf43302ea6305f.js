82e89f4cbfc9afb845a7b9fd3030873abaca3f3b
unknown <mariia>
2019-07-26 21:26:32 +0300

    Updates

---
+++ precache-manifest.002973ad271490daaaaf43302ea6305f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a552fd3daa80c420eb580f1153383638",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "27eaab7403cc36f9a140",
+    "url": "/react_todo-app/static/css/main.0d33e2de.chunk.css"
+  },
+  {
+    "revision": "efd10c59a326dcc849c1",
+    "url": "/react_todo-app/static/js/2.284282ac.chunk.js"
+  },
+  {
+    "revision": "27eaab7403cc36f9a140",
+    "url": "/react_todo-app/static/js/main.b25c2f30.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
